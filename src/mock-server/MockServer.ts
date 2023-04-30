79e069b9a93e3c369a1a00aabd6a1a9a751e9b78
Gleb <M20XV@yandex.ru>
2023-04-30 16:16:30 +0300

    implement subscriptions

--- src/mock-server/MockServer.ts
+++ src/mock-server/MockServer.ts
@@ -13,14 +13,13 @@ function randInt(min = 0, max = 100) {
 }
 
 const sides = ['buy', 'sell'] as const
-const statuses = ['active', 'filled', 'rejected', 'cancelled'] as const
 function randOrder() {
   const now = new Date()
   return new Order(
     id(),
     now,
     now,
-    statuses[randInt(0, 4)],
+    'active',
     sides[randInt(0, 2)],
     randInt(500, 3000),
     randInt(1000) * randInt(0, 1000),
@@ -33,9 +32,10 @@ export class MockServer {
     Array(5).fill(0).map(randOrder),
     (order) => order.id
   )
+  private readonly _priceSubs = new Map<string, string[]>()
   //instrument - price
-  private readonly instrumentSellPrices: Map<string, number> = new Map()
-  private readonly instrumentBuyPrices: Map<string, number> = new Map()
+  private readonly _instrumentSellPrices: Map<string, number> = new Map()
+  private readonly _instrumentBuyPrices: Map<string, number> = new Map()
 
   constructor(private readonly _connection: MockConnection) {
     _connection.outMessages$.subscribe(this.onMessage.bind(this))
@@ -65,16 +65,43 @@ export class MockServer {
         const filled = order.copy({ status: 'filled' })
         this._orders.set(filled.id, filled)
         this._sendOrders([filled])
+
+        const instrument = msg.message.instrument
+        this._setInstrumentSellPrices(instrument, msg.message.price)
+        this._setInstrumentBuyPrices(instrument, msg.message.price)
+        this._notifyPriceSubs(instrument)
       } else {
         const rejected = order.copy({ status: 'rejected' })
         this._orders.set(rejected.id, rejected)
         this._sendOrders([rejected])
       }
+    } else if (msg.messageType == MessageType.Subscribe) {
+      const id = this._addSub(msg.message)
+      this.send({
+        messageType: MessageType.SubscribeResult,
+        message: { type: 'ok', subId: id },
+      })
+    } else if (msg.messageType == MessageType.Unsubscribe) {
+      this._removeSub(msg.message.subId)
     }
   }
 
-  send(msg: Message) {
+  private send(msg: Message) {
     this._connection.outSend(msg)
+  }
+
+  private _setInstrumentSellPrices(instrument: string, price: number) {
+    this._instrumentSellPrices.set(
+      instrument,
+      this._instrumentSellPrices.get(instrument) ?? price
+    )
+  }
+
+  private _setInstrumentBuyPrices(instrument: string, price: number) {
+    this._instrumentBuyPrices.set(
+      instrument,
+      this._instrumentBuyPrices.get(instrument) ?? price
+    )
   }
 
   private _sendOrders(orders: Order[]) {
@@ -90,6 +117,42 @@ export class MockServer {
         side: order.side,
         status: order.status,
       })),
+    })
+  }
+
+  private _addSub(instrument: string) {
+    const subs = this._priceSubs.get(instrument) ?? []
+    const subId = id()
+    subs.push(subId)
+    this._priceSubs.set(instrument, subs)
+    return subId
+  }
+
+  private _removeSub(id: string) {
+    const subs = [...this._priceSubs.keys()]
+    subs.forEach((instrument) => {
+      const subs = this._priceSubs.get(instrument)!
+      this._priceSubs.set(
+        instrument,
+        subs.filter((subId) => subId != id)
+      )
+    })
+  }
+
+  private _notifyPriceSubs(instrument: string) {
+    const subs = this._priceSubs.get(instrument) ?? []
+    const sell = this._instrumentSellPrices.get(instrument) ?? 0
+    const buy = this._instrumentBuyPrices.get(instrument) ?? 0
+
+    subs.forEach((subId) => {
+      this.send({
+        messageType: MessageType.InstrumentPricesUpdate,
+        message: {
+          instrument,
+          newBuy: buy,
+          newSell: sell,
+        },
+      })
     })
   }
 
